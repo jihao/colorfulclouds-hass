30bbe685283295903d311a0887a34fcc84dc3db2
Hao Ji <jacky.jihao@gmail.com>
2019-06-09 18:49:41 +0800

    Update __init__.py

--- custom_components/colorfulclouds/__init__.py
+++ custom_components/colorfulclouds/__init__.py
@@ -2,5 +2,5 @@
 Component to integrate with 彩云天气.
 
 For more details about this component, please refer to
-https://github.com/jihao/colorfulclouds
+https://github.com/jihao/colorfulclouds-hass
 """
